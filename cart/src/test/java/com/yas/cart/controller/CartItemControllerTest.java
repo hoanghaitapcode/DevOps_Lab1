7fbb2d070d143d8d6e00bfa9f187b1c11aa033fe
TNhanTis <trongnhan18052005@gmail.com>
2026-05-03 16:30:26 +0700

    cart

--- cart/src/test/java/com/yas/cart/controller/CartItemControllerTest.java
+++ cart/src/test/java/com/yas/cart/controller/CartItemControllerTest.java
@@ -60,7 +60,7 @@ class CartItemControllerTest {
                 .quantity(1);
         }
 
-
+        //shibaloncha
         @Test
         void testAddToCart_whenProductIdIsNull_shouldReturnBadRequest() throws Exception {
             CartItemPostVm cartItemPostVm = cartItemPostVmBuilder.productId(null).build();
