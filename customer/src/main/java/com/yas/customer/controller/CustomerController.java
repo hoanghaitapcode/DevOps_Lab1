44cd3ce01832f1be6ac84b85e16bcc0dc7483059
TNhanTis <trongnhan18052005@gmail.com>
2026-05-03 22:45:12 +0700

    customer

--- customer/src/main/java/com/yas/customer/controller/CustomerController.java
+++ customer/src/main/java/com/yas/customer/controller/CustomerController.java
@@ -16,7 +16,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;//shiba
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
